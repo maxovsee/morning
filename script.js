818ecfbaa4cb6f8d3a6c8ea089837f4b410c6e07
maxovsee <80675065+maxovsee@users.noreply.github.com>
2024-07-04 16:22:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
- 
+<script>
+   let button = document.getElementById("scroll-button");
+   let windowHeight = window.innerHeight;
+   let documentHeight = document.body.offsetHeight;
+   window.addEventListener("scroll", function() {
+     let scrollPosition = window.scrollY;
+     if (scrollPosition + windowHeight >= documentHeight) {
+       button.style.top = "20px";
+       button.style.bottom = "auto";
+       button.style.display = "block"; // show the button
+     } else if (scrollPosition <= 0) {
+       button.style.top = "auto";
+       button.style.bottom = "20px";
+       button.style.display = "block"; // show the button
+     } else {
+       button.style.display = "none"; // hide the button
+     }
+   });
+   function scrollToTopOrBottom() {
+     let scrollPosition = window.scrollY;
+     if (scrollPosition + windowHeight >= documentHeight) {
+       window.scrollTo({ top: 0, behavior: 'smooth' });
+     } else if (scrollPosition <= 0) {
+       window.scrollTo({ top: documentHeight - windowHeight, behavior: 'smooth' });
+     }
+   }
+  
+  const mondayText = document.getElementById("monday-text");
+
+  const currentDayPravila = new Date().getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+
+  if (currentDayPravila === 1) { // 1 = Monday
+    mondayText.style.display = "block";
+  } else {
+    mondayText.style.display = "none";
+  }
+  
+
+  
+  // каноны по одному в день
+  const kanons = document.querySelectorAll('.kanon');
+let currentDay = new Date().getDate(); // 1-31
+
+let startIndex = (currentDay - 1) % kanons.length;
+
+kanons.forEach((kanon, index) => {
+  if (index === startIndex) {
+    kanon.style.display = 'block';
+  } else {
+    kanon.style.display = 'none';
+  }
+});
+
+
+
+</script>
