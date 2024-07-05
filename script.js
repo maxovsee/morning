318a4cdf2d15362fff490626de09aec16982ca76
maxovsee <80675065+maxovsee@users.noreply.github.com>
2024-07-06 01:45:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,13 +73,13 @@ kathismas.forEach((kathisma, index) => {
 
     timer = setTimeout(() => {
       const currentTime = new Date().getTime();
-      if (currentTime - startTime >= 5000) { // 5 seconds
+      if (currentTime - startTime >= 3000) { // 5 seconds
         const kathismaTolkId = `kathisma-tolk-${index + 1}`;
         const kathismaTolkElement = document.getElementById(kathismaTolkId);
         kathisma.outerHTML = kathismaTolkElement.outerHTML;
         kathisma.style.display = 'block'; // ensure the swapped element is visible
       }
-    }, 5000); // 5 seconds
+    }, 3000); // 5 seconds
 
     kathisma.addEventListener('touchend', () => {
       clearTimeout(timer);
