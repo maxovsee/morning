f6c45b56a25870b091817f4d0b1dc2038b667b79
maxovsee <80675065+maxovsee@users.noreply.github.com>
2024-08-17 11:18:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const jsonDataElement = document.getElementById('json-data');
 let jsonData = [];
 
 // Load JSON data from file (assuming it's named data.json)
-fetch('data.json?t=' + new Date().getTime())
+fetch('data.json')
   .then(response => response.json())
   .then(data => {
     jsonData = data;
